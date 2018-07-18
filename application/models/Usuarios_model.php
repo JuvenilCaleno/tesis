c14f1ff7b1af29d6770adda36d49a56217ac99fc
JuvenilCaleno <41033562+JuvenilCaleno@users.noreply.github.com>
2018-07-18 18:14:52 -0500

    Update Usuarios_model.php

--- application/models/Usuarios_model.php
+++ application/models/Usuarios_model.php
@@ -3,7 +3,7 @@
 class Usuarios_model extends CI_Model {
 
     public function buscar_autentificacion($usuario, $clave) {
-        $user = $this->db->select('*')->get_where('admin_usuarios', array(
+        $user = $this->db->select('*')->get_where('ADMIN_USUARIOS', array(
                     'usuario' => $usuario,
                     'clave' => $clave
                 ))->row();
@@ -12,7 +12,7 @@ class Usuarios_model extends CI_Model {
 
     public function get_usuarios() {
         $this->db->select('*');
-        $this->db->from('admin_usuarios');
+        $this->db->from('ADMIN_USUARIOS');
         //$this->db->join('admin_roles', 'admin_usuarios.ID_ROL = admin_roles.ID_ROL');
         $consulta = $this->db->get();
         return $consulta->result();
